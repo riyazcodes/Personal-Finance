3b8db28a170636ba5c2767bb894b1e1ee09b04a7
riyazcodes <108416921+riyazcodes@users.noreply.github.com>
2022-07-28 20:52:44 -0400

    cleaned up

--- .vscode/taxes.js
+++ .vscode/taxes.js
@@ -7,13 +7,12 @@ let deductions= standardDeduction + fouroOneK + otherDeductions;
 let taxableIncome= netIncome-deductions;
 
 function calcTaxes(taxableIncome) {
-    let unadressedMoney=taxableIncome
     let taxAmt = 0;
     let myTaxBracket= 1
     const bracket= [9950,40525,86375,170050,215950,539900];
     const rates = [0.10,0.12,0.22,0.24,0.32,0.35,0.37];
-    if (unadressedMoney>9950) {taxAmt += bracket[0]*rates[0]}
-      else (taxAmt += (unadressedMoney * rates[0]))
+    if (taxableIncome>bracket[0]) {taxAmt += bracket[0]*rates[0]}
+      else (taxAmt += (taxableIncome * rates[0]))
     for (let i=1;taxableIncome>=bracket[i];i++) {
     taxAmt += ((bracket[i]-bracket[i-1])*(rates[i])); myTaxBracket++};
     taxAmt += ((taxableIncome-bracket[myTaxBracket-1])*rates[myTaxBracket]);
